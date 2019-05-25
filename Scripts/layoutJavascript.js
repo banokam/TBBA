e22910d11772bdc7eda446acb0a377df3f32c9bc
Bryant Anokam <ianokam.math@gmail.com>
2019-05-25 13:11:15 -0400

    site folders and index added
    html, css, javascript, image and video files have been added.

---
+++ Scripts/layoutJavascript.js
@@ -0,0 +1,173 @@
+/*---------------------------------------------------------------------*/
+/* ---------- [[[    MENU TOGGLE ON/OFF    ]]] ------------------------*/
+
+$( "button" ).click(function() {
+    var win = $(this); //this = window
+    var size=$( "body" ).height();
+    $("#menuBarBlack").css("height", size + "px");
+
+
+    $( "#menuBarBlack" ).fadeToggle( "slow", "linear" );
+    $( ".pos-f-t" ).fadeToggle( "slow", "linear" );
+  });
+/*------------------------------------------------------*/
+  $( ".fadeToggle" ).click(function() {
+    var win = $(this); //this = window
+    var size=$( "body" ).height();
+    $("#menuBarBlack").css("height", size + "px");
+
+
+    $( "#menuBarBlack" ).fadeToggle( "slow", "linear" );
+    $( ".pos-f-t" ).fadeToggle( "slow", "linear" );
+  });
+  
+/*-------------------------------------------------------*/  
+/* ---------- [[[    TABS    ]]] ------------------------*/
+function openHome() {
+    /* --------------CHANGE CSS STYLE----------------- */
+    /*TURN ON/OFF HOME TAB CONTENT/OTHER TAB CONTENT*/
+    $("#tabContent").css("display", "none");
+    $("#tabHome").css("display", "initial");
+
+    // if($( window ).width() > 767){
+    //     $("#homeBackground").css("display", "initial");
+    //     $("#homeBackground2").css("display", "none");
+    //     $("#homeBackground3").css("display", "none");
+    // }
+    // else if($( window ).width() <= 767){
+    //     $("#homeBackground").css("display", "none");
+    //     $("#homeBackground2").css("display", "initial");
+    //     $("#homeBackground3").css("display", "none");
+    // }
+    // else if($( window ).width() <= 664){
+    //     $("#homeBackground").css("display", "none");
+    //     $("#homeBackground2").css("display", "none");
+    //     $("#homeBackground3").css("display", "initial");
+    // }
+
+    // $("#contentBackground").css("display", "none");
+
+    /*--------------------------------------*/
+    /*TURN ON/OFF TAB CONTENT*/
+    $("#coding").css("display", "none");
+    $("#photo").css("display", "none");
+    $("#illustration").css("display", "none");
+    $("#math-website").css("display", "none");
+
+    /* --------------CHANGE HTML ATTR----------------- */
+    /*CHANGE TAB ACTIVE/NONACTIVE LOOK*/
+    $("#homeTab").attr("class", "nav-item nav-link  active");
+    $("#codeTab").attr("class", "nav-item nav-link  unactive");
+    $("#photoTab").attr("class", "nav-item nav-link  unactive");
+    $("#illustrationTab").attr("class", "nav-item nav-link  unactive");
+    $("#mathWebsiteTab").attr("class", "nav-item nav-link  unactive");
+    
+    $("#homeTab2").attr("class", "activeTab");
+    $("#codeTab2").attr("class", "unactiveTab");
+    $("#photoTab2").attr("class", "unactiveTab");
+    $("#illustrationTab2").attr("class", "unactiveTab");
+    $("#mathWebsiteTab2").attr("class", "unactiveTab");
+
+}
+
+function openCode() {
+    /* --------------CHANGE CSS STYLE----------------- */
+    $("#tabContent").css("display", "initial");
+    $("#tabHome").css("display", "none");
+    // $("#homeBackground").css("display", "none");
+    // $("#contentBackground").css("display", "initial");
+    /*--------------------------------------*/
+    $("#coding").css("display", "initial");
+    $("#photo").css("display", "none");
+    $("#illustration").css("display", "none");
+    $("#math-website").css("display", "none");
+    /* --------------CHANGE HTML ATTR----------------- */
+    $("#homeTab").attr("class", "nav-item nav-link  unactive");
+    $("#codeTab").attr("class", "nav-item nav-link  active");
+    $("#photoTab").attr("class", "nav-item nav-link  unactive");
+    $("#illustrationTab").attr("class", "nav-item nav-link  unactive");
+    $("#mathWebsiteTab").attr("class", "nav-item nav-link  unactive");
+    
+    $("#homeTab2").attr("class", "unactiveTab");
+    $("#codeTab2").attr("class", "activeTab");
+    $("#photoTab2").attr("class", "unactiveTab");
+    $("#illustrationTab2").attr("class", "unactiveTab");
+    $("#mathWebsiteTab2").attr("class", "unactiveTab");
+
+}
+
+function openPhoto() {
+    $("#tabContent").css("display", "initial");
+    $("#tabHome").css("display", "none");
+    // $("#homeBackground").css("display", "none");
+    // $("#contentBackground").css("display", "initial");
+    /*--------------------------------------*/
+    $("#coding").css("display", "none");
+    $("#photo").css("display", "initial");
+    $("#illustration").css("display", "none");
+    $("#math-website").css("display", "none");
+    /* ------------------------------------- */
+    $("#homeTab").attr("class", "nav-item nav-link  unactive");
+    $("#codeTab").attr("class", "nav-item nav-link  unactive");
+    $("#photoTab").attr("class", "nav-item nav-link  active");
+    $("#illustrationTab").attr("class", "nav-item nav-link  unactive");
+    $("#mathWebsiteTab").attr("class", "nav-item nav-link  unactive");
+
+    $("#homeTab2").attr("class", "unactiveTab");
+    $("#codeTab2").attr("class", "unactiveTab");
+    $("#photoTab2").attr("class", "activeTab");
+    $("#illustrationTab2").attr("class", "unactiveTab");
+    $("#mathWebsiteTab2").attr("class", "unactiveTab");
+
+}
+
+function openIllustration() {
+    $("#tabContent").css("display", "initial");
+    $("#tabHome").css("display", "none");
+    // $("#homeBackground").css("display", "none");
+    // $("#contentBackground").css("display", "initial");
+    /*--------------------------------------*/
+    $("#coding").css("display", "none");
+    $("#photo").css("display", "none");
+    $("#illustration").css("display", "initial");
+    $("#math-website").css("display", "none");
+    /* ------------------------------------- */
+    $("#homeTab").attr("class", "nav-item nav-link  unactive");
+    $("#codeTab").attr("class", "nav-item nav-link  unactive");
+    $("#photoTab").attr("class", "nav-item nav-link  unactive");
+    $("#illustrationTab").attr("class", "nav-item nav-link  active");
+    $("#mathWebsiteTab").attr("class", "nav-item nav-link  unactive");
+
+    $("#homeTab2").attr("class", "unactiveTab");
+    $("#codeTab2").attr("class", "unactiveTab");
+    $("#photoTab2").attr("class", "unactiveTab");
+    $("#illustrationTab2").attr("class", "activeTab");
+    $("#mathWebsiteTab2").attr("class", "unactiveTab");
+
+}
+
+function openMathWebsite() {
+    $("#tabContent").css("display", "initial");
+    $("#tabHome").css("display", "none");
+    // $("#homeBackground").css("display", "none");
+    // $("#contentBackground").css("display", "initial");
+    /*--------------------------------------*/
+    $("#coding").css("display", "none");
+    $("#photo").css("display", "none");
+    $("#illustration").css("display", "none");
+    $("#math-website").css("display", "initial");
+    /* ------------------------------------- */
+    $("#homeTab").attr("class", "nav-item nav-link  unactive");
+    $("#codeTab").attr("class", "nav-item nav-link  unactive");
+    $("#photoTab").attr("class", "nav-item nav-link  unactive");
+    $("#illustrationTab").attr("class", "nav-item nav-link  unactive");
+    $("#mathWebsiteTab").attr("class", "nav-item nav-link  active");
+
+    $("#homeTab2").attr("class", "unactiveTab");
+    $("#codeTab2").attr("class", "unactiveTab");
+    $("#photoTab2").attr("class", "unactiveTab");
+    $("#illustrationTab2").attr("class", "unactiveTab");
+    $("#mathWebsiteTab2").attr("class", "activeTab");
+
+
+}
